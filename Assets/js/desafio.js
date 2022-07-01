285136d4e7928fb8a71ecaafc128f562d12e732d
María Paz Labbé <104472337+MariaPazLabbe@users.noreply.github.com>
2022-07-01 19:32:13 -0400

    add description let

--- Assets/js/desafio.js
+++ Assets/js/desafio.js
@@ -1,4 +1,4 @@
-document.querySelector("#button").addEventListener("click", ()=>{
+let button = document.querySelector("#button");document.querySelector("#button").addEventListener("click", () => {
     document.querySelector('p .total').innerHTML = document.querySelector("#precio").value*document.querySelector('#var-1').value;
     document.querySelector('p .cantidad').innerHTML = document.querySelector('#var-1').value;
     document.querySelector('p .color_laptopt').style.color = document.querySelector('#var-2').value;
