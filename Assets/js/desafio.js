3c473cbab7523ff0687324a67222b38e340122c8
María Paz Labbé <104472337+MariaPazLabbe@users.noreply.github.com>
2022-07-01 19:00:45 -0400

    Correct .color_laptopt

--- Assets/js/desafio.js
+++ Assets/js/desafio.js
@@ -1,5 +1,5 @@
 document.querySelector("#button").addEventListener("click", ()=>{
     document.querySelector('p .total').innerHTML = document.querySelector("#precio").value*document.querySelector('#var-1').value;
     document.querySelector('p .cantidad').innerHTML = document.querySelector('#var-1').value;
-    document.querySelector('p .color_laptop').style.color = document.querySelector('#var-2').value;
+    document.querySelector('p .color_laptopt').style.color = document.querySelector('#var-2').value;
 })
