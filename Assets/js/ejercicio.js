67dc2670036931f670d6cbff64c0b80b52625a93
María Paz Labbé <104472337+MariaPazLabbe@users.noreply.github.com>
2022-07-01 18:31:15 -0400

    practical exercise draft

--- Assets/js/ejercicio.js
+++
@@ -1,9 +0,0 @@
-num1 = prompt("Ingrese el primer número");
-num2 = prompt("Ingrese el segundo número");
-
-resultado = Number(num1)+ Number(num2);
-alert(resultado);
-
-
-// nombre = prompt("Ingresa tu nombre");
-// alert("Hola" + " " + nombre);
