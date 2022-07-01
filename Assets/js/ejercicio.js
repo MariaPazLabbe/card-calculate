5e432ab05e46bd1dcb499d0fe95ff332c2358682
Maria Paz Labbe <malabbeg@udd.cl>
2022-07-01 18:25:06 -0400

    first commit

---
+++ Assets/js/ejercicio.js
@@ -0,0 +1,9 @@
+num1 = prompt("Ingrese el primer número");
+num2 = prompt("Ingrese el segundo número");
+
+resultado = Number(num1)+ Number(num2);
+alert(resultado);
+
+
+// nombre = prompt("Ingresa tu nombre");
+// alert("Hola" + " " + nombre);
