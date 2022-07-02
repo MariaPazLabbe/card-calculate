fac0d4b57357538e983de7640057ad92c0e1ae1e
María Paz Labbé <104472337+MariaPazLabbe@users.noreply.github.com>
2022-07-01 20:29:32 -0400

    change letters EN

--- Assets/js/desafio.js
+++ Assets/js/desafio.js
@@ -1,5 +1,5 @@
 let button = document.querySelector("#button");document.querySelector("#button").addEventListener("click", () => {
-    document.querySelector('p .total').innerHTML = document.querySelector("#precio").value*document.querySelector('#var-1').value;
+    document.querySelector('p .total').innerHTML = document.querySelector("#price").value*document.querySelector('#var-1').value;
     document.querySelector('p .amount').innerHTML = document.querySelector('#var-1').value;
     document.querySelector('p .color_laptop').style.color = document.querySelector('#var-2').value;
 })
